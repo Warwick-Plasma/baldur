c2f6d8cf73caa9a41ae9c5f3383ba90b65c17b3d
Duncan Barlow (VB) <d.barlow@warwick.ac.uk>
2020-06-04 16:38:56 +0100

    Add warning if no ray or path data is found

--- odin_plot.py
+++ odin_plot.py
@@ -473,12 +473,18 @@ def snapshot(dat, fig, ax1, cax1, var_name, *args, **kwargs):
       skip = 1
       plot_rays('Beam1', 'Energy', skip, dat, fig, ax1, parameters.use_polar,
                 grid_conv)
+    else:
+      print(" ")
+      print("No light ray data found")
     if hasattr(dat, 'Burst1'):
       num_burs = len(dat.bursts)
       for iname in range(0, num_burs):
         skip = 1
         plot_rays(dat.bursts[iname], 'Energy_Deposited', skip, dat, fig, ax1,
                   parameters.use_polar, grid_conv)
+    else:
+      print(" ")
+      print("No electron path data found")
 
   ax1.set_xlabel(x_label, fontsize = fs)
   ax1.set_ylabel(y_label, fontsize = fs)
